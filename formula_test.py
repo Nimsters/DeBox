4d295553e3de4c27eae3a61d321e6b7c64de4883
Nim <nim@nimsters.private>
2016-02-19 02:54:45 +0100

    Got rid of more brackets and -ironically- added a bracketing function

--- formula_test.py
+++ formula_test.py
@@ -1,31 +1,41 @@
 def unparse(root):
+    def bracket(t):
+        return "("+t+")"
+    
     def infix(t, subtree):
         if len(subtree)!=2:
-            string = "**error: \""+t+"\" not given two subtrees**"
-        else:
-            string = "("+unparse(subtree[0]) + t + unparse(subtree[1])+")"
-        return string
+            return "**error: \""+t+"\" not given two subtrees**"
+        (c1,c2) = (subtree[0].get('s',"!"),subtree[1].get('s',"!"))
+        (first, second) = (unparse(subtree[0]),unparse(subtree[1]))
+        if c1 not in ['*','~']:
+            first = bracket(first)
+        if c2 not in ['*','~']:
+            second = bracket(second)
+        return first + t + second
 
     def prefix(t, subtree):
         if len(subtree)!=1:
-            string = "**error: \""+t+"\" not given one subtree**"
+            return "**error: \""+t+"\" not given one subtree**"
+        sub = unparse(subtree[0])
+        c = subtree[0].get('s',"!")
+        if c not in ['*', 'not']:
+            string = t+" "+bracket(sub)
         else:
-            string = t + "(" + unparse(subtree[0]) + ")"
+            string = t +"-"+sub
         return string
 
     def implies(t, subtree):
         if len(subtree)!=2:
-            string = "**error: \""+t+"\" not given two subtrees**"
+            return "**error: \""+t+"\" not given two subtrees**"
+        (c1,c2) = (subtree[0].get('s',"!"),subtree[1].get('s',"!"))
+        if (c1 == "*" and c2=="*"):
+            string = unparse(subtree[0]) + t + unparse(subtree[1])
         else:
-            (c1,c2) = (subtree[0].get('s',"!"),subtree[1].get('s',"!"))
-            if (c1 == "*" and c2=="*"):
-                string = unparse(subtree[0]) + t + unparse(subtree[1])
-            else:
-                mod1 = " does not hold " if c1=='~' else " holds "
-                mod2 = " does not hold" if c1=='~' else " holds"
-                if c2 == "F": mod2 = " is implied"
-                string = ("if "   + unparse(subtree[0]) + mod1 +
-                          "then " + unparse(subtree[1]) + mod2)
+            mod1 = " does not hold " if c1=='~' else " holds "
+            mod2 = " does not hold" if c2=='~' else " holds"
+            if c2 == "F": mod2 = " is implied"
+            string = ("if "   + unparse(subtree[0]) + mod1 +
+                      "then " + unparse(subtree[1]) + mod2)
         return string
     
     def const(t, subtree):
@@ -35,10 +45,10 @@ def unparse(root):
         '&' : (" and ", infix),
         '/' : (" or ", infix),
         '>' : (" implies ", implies),
-        '~' : ("not ", prefix),
+        '~' : ("not", prefix),
         'F' : ("a contradiction", const),
         '*' : (root.get('atom', "**error: atom not specified**"), const),
-         1  : ("**error**", const),
+         1  : ("**error:symbol not specified**", const),
     }
 
     (text, function) = symbols.get(root.get('s', 1), 
@@ -57,7 +67,7 @@ if __name__ == "__main__":
     tree6 = {'s':">", 'subtree':[tree2,tree4]}
     tree7 = {'s':">", 'subtree':[{'s':"&", 'subtree':[p,tree1]},{'s':"F"}]}
    
-    for test in [p,tree1, tree2, tree3, tree4, tree5, tree6, tree7]:
+    for test in [p,tree1, tree2, tree4, tree5, tree6, tree7]:
         print ("The parse tree ")
         print (test)
         print ("is rendered as follows:\n")
